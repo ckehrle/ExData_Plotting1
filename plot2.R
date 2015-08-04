a46dc78bcaa911fd7ff7a9f1d974803cc3e620d2
ckehrle <christian.kehrle@gmx.de>
2015-08-04 20:00:49 +0200

    First script revised and second plot

--- plot2.R
+++ plot2.R
@@ -7,20 +7,16 @@ library(lubridate)
 
 powr_consump_fil <- tbl_df(read.csv("./data/household_power_consumption.txt",header=TRUE,sep=";",na.strings="?",stringsAsFactors=FALSE)) %>%
     filter(dmy(Date) %in% c(ymd('2007/02/01'),ymd('2007/02/02'))) %>% 
-    mutate(Date = dmy(Date))      
+    mutate(Date = parse_date_time(paste(Date,Time),"%d%m%y %H%M%S"))   
 
-png(filename = "plot1.png",
+png(filename = "plot2.png",
     width = 480, height = 480, units = "px", pointsize = 12,
     bg = "white", res = NA, family = "", restoreConsole = TRUE,
     type = c("windows", "cairo", "cairo-png"))
 
-hist(powr_consump_fil$Global_active_power,
-        ylab="Frequeny",
-        xlab="Global active power (kilowatts)",
-        col="red",
-        main=" Global active Power",
-        ylim=c(0,1200)
-
-     )
+plot(powr_consump_fil$Global_active_power ~ powr_consump_fil$Date,
+     type="l",
+     ylab="Global active Power",
+     xlab="")
+     
 dev.off()
-        
